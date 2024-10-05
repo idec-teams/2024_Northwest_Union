2880cbcdb07a3057591af90231465f4dcda8fed2
cyanineeee <sapphire_ruby@163.com>
2024-10-06 01:56:46 +0800

    explore-flask-freeze

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from flask_frozen import Freezer
 template_folder = path.abspath('./wiki')
 
 app = Flask(__name__, template_folder=template_folder)
-# app.config['FREEZER_DESTINATION'] = 'public'
+app.config['FREEZER_DESTINATION'] = 'public'
 app.config['FREEZER_RELATIVE_URLS'] = True
 app.config['FREEZER_IGNORE_MIMETYPE_WARNINGS'] = True
 freezer = Freezer(app)
@@ -25,9 +25,17 @@ def serve():
 def home():
     return render_template('pages/home.html')
 
-@app.route('/<page>')
+@app.route('/<page>/', defaults={'page': 'home'})  # 默认页面为 home
+@app.route('/<page>.html')
 def pages(page):
-    return render_template(str(Path('pages')) + '/' + page.lower() + '.html')
+    return render_template('pages/' + page.lower() + '.html')
+
+# URL 生成器，确保生成正确的静态页面
+# @freezer.register_generator
+# def page_urls():
+#     yield 'pages', {'page': 'home'}
+#     # yield 'pages', {'page': 'about'}
+#     # 其他页面按需添加
 
 if __name__ == "__main__":
     freezer.freeze()
