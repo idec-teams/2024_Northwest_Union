8ddb15008005abf11d93df30f29c39da37db1836
cyanineeee <sapphire_ruby@163.com>
2024-10-06 01:22:42 +0800

    add "Freezer(app).freeze()"

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from pathlib import Path
 from flask import Flask, render_template
 from flask_frozen import Freezer
 
-
 template_folder = path.abspath('./wiki')
 
 app = Flask(__name__, template_folder=template_folder)
+
 @app.route('/')
 def home():
     return render_template('pages/home.html')
@@ -17,7 +17,7 @@ def pages(page):
     return render_template(str(Path('pages')) + '/' + page.lower() + '.html')
 
 if __name__ == '__main__':
-    Freezer.freeze()
+    Freezer(app).freeze()
     # app.run(port=8080,debug=True)
 
 
