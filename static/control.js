e97bcc859041bd7858150c229020138e13a7d6ee
cyanineeee <cyanineeee@users.noreply.github.com>
2024-10-07 16:34:44 +0000

    deploy: daa8b507bf2f664616de0ab1c2784dac5c2e1126

---
+++ static/control.js
@@ -0,0 +1,35 @@
+window.onload = () => {
+    const bgHero = document.getElementsByClassName("bg-hero");
+    let topGap = bgHero[0].getBoundingClientRect().top;
+    const toTop = bgHero[0].getElementsByClassName("to-top")[0];
+
+    judgeToTop(topGap,bgHero[0]);
+    const body = document.getElementsByTagName("body")[0];
+    console.log(body)
+
+    console.log(bgHero[0]);
+    if(bgHero[0].getElementsByClassName("lead")[0].innerHTML){
+        bgHero[0].style.top = "-198px"
+    }
+
+    body.onscroll = () => {
+        topGap = bgHero[0].getBoundingClientRect().top;
+        judgeToTop(topGap,bgHero[0]);
+    }
+
+    toTop.addEventListener("click",() => {
+        console.log("Asd")
+        document.documentElement.scrollTo(0,0)
+    })
+
+
+}
+
+function judgeToTop(topGap,bgHero){
+    if(topGap == -198 || topGap == -150){
+        bgHero.getElementsByClassName("to-top")[0].style.display="block";
+    }
+    if(topGap == 56){
+        bgHero.getElementsByClassName("to-top")[0].style.display="none";
+    }
+}
